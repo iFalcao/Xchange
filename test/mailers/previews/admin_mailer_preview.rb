7f68fa67a5a14486684e75d657a636dd86f9c874
Icaro Falcao <vbsfalcao@gmail.com>
2017-06-15 13:12:35 -0300

    Treat errors from sending emails and sets notify accordingly + creates send_message preview

--- test/mailers/previews/admin_mailer_preview.rb
+++ test/mailers/previews/admin_mailer_preview.rb
@@ -4,4 +4,7 @@ class AdminMailerPreview < ActionMailer::Preview
         AdminMailer.update_email(Admin.all.sample, Admin.last)
     end
 
+    def send_message
+        AdminMailer.send_message(Admin.all.sample, Admin.last, "subject text", "message text")
+    end
 end
